7aed194f027db45a16b02a98851bf82190d868b1
Apur0y <apuroy2785@gmail.com>
2025-04-27 19:16:58 +0600

    fixed the logo

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.getElementById("hamburger");
-  const navbar = document.getElementById("navbar");
-
-  hamburger.addEventListener("click", () => {
-    navbar.classList.toggle("active");
-  });
-});
 
 // Features
 
